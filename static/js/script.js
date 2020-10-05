327c10a7ec24009b5467733b79af78430058dad5
wolf811 <wolf811@gmail.com>
2020-10-05 19:29:41 +0300

    add post_detail

--- static/js/script.js
+++ static/js/script.js
@@ -44,3 +44,48 @@ $('#logOff').click(function() {
     $('#formAuthorization').show();
 })
 // END AUTORIZATION
+
+
+// Clipping text
+$.fn.succinct = function(options) {
+
+    var settings = $.extend({
+        size: 240,
+        omission: '...',
+        ignore: true
+    }, options);
+
+    return this.each(function() {
+
+        var textDefault,
+            textTruncated,
+            elements = $(this),
+            regex = /[!-\/:-@\[-`{-~]$/,
+            init = function() {
+                elements.each(function() {
+                    textDefault = $(this).html();
+
+                    if (textDefault.length > settings.size) {
+                        textTruncated = $.trim(textDefault)
+                            .substring(0, settings.size)
+                            .split(' ')
+                            .slice(0, -1)
+                            .join(' ');
+
+                        if (settings.ignore) {
+                            textTruncated = textTruncated.replace(regex, '');
+                        }
+
+                        $(this).html(textTruncated + settings.omission);
+                    }
+                });
+            };
+        init();
+    });
+};
+
+$('.truncate').succinct({
+    size: 200,
+});
+
+// END Clipping text
