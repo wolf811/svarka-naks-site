d740e4e15f6d4277048a6ce8f8b23e4f9450667a
wolf811 <wolf811@gmail.com>
2020-10-06 19:52:36 +0300

    five pages

--- journal/urls.py
+++ journal/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.home, name='home'),
+    path('post_list', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('journal_info', views.journal_info, name='journal_info'),
+    path('journal_founders', views.journal_founders, name='journal_founders'),
+    path('journal_collegium', views.journal_collegium, name='journal_collegium'),
+    path('alyoshin', views.alyoshin, name='alyoshin'),
 ]
