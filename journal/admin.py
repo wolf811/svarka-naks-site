3cd834ea8f0f0620f5f0747c3c2f98fd0e4fc118
wolf811 <wolf811@gmail.com>
2020-10-26 18:59:10 +0300

    add model Product

--- journal/admin.py
+++ journal/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Product
 
 # Register your models here.
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published_date', 'author')
+admin.site.register(Post, PostAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'created', 'updated']
+    list_filter = ['created', 'updated']
+    list_editable = ['price']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Product, ProductAdmin)
--- journal/models.py
+++ journal/models.py
@@ -13,6 +13,7 @@ class Post(models.Model):
     published_date = models.DateTimeField(verbose_name='Дата публикации', blank=True, null=True)
     img = models.ImageField(upload_to='images/', verbose_name='Добавить фото', null=True, blank=True)
     img_journal = models.ImageField(upload_to='images/', verbose_name='Добавить фото журнала', null=True, blank=True)
+    file_contents = models.FileField(upload_to='uploads/', verbose_name='Добавить файл содержания', null=True, blank=True)
 
     @property
     def img_url(self):
@@ -30,3 +31,35 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+# class Category(models.Model):
+#     name = models.CharField(max_length=200, db_index=True)
+#     slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+#     class Meta:
+#         ordering = ('name',)
+#         verbose_name = 'Категория'
+#         verbose_name_plural = 'Категории'
+
+#     def __str__(self):
+#         return self.name
+
+
+class Product(models.Model):
+    # category = models.ForeignKey(Category, related_name='products')
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=0)
+    # stock = models.PositiveIntegerField()
+    # available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
